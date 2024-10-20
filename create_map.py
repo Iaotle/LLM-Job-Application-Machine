0be882c153038b059527626d2a17903bcae066c4
Vadim Isakov <vintersiaotle@gmail.com>
2024-10-20 19:39:33 +0200

    automatically fetch location data from JSON files

--- create_map.py
+++ create_map.py
@@ -4,6 +4,7 @@ import os
 import geopy
 import folium
 import webbrowser
+import json
 
 # Create a geocoder object
 geolocator = geopy.Nominatim(user_agent="Job_scraper_2024")
@@ -11,17 +12,31 @@ geolocator = geopy.Nominatim(user_agent="Job_scraper_2024")
 # # Ask the user to input multiple locations separated by commas
 # locations = input("Enter multiple locations separated by commas: ").split(',')
 
-locations = [
+home_locations = [
     'Darlingstraat 983  1102MX',
     'Dennenrodepad 15   1102MW'
 ]
-    
+
+
+# load json from companies/
+# json will look like this:
+# {
+#     "name": "Vripack Design B.V.",
+#     "kvk": "01035286",
+#     "website": "https://vripack.com/",
+#     "careers_page": null,
+#     "address": "Oppenhuizerweg 2, 8606 AS Sneek",
+#     "sector": "Architecture"
+# }
+# empty parts are = null
+# generate a map of companies if they have an address that isn't null and add them to the map
+# {code}
 map = folium.Map(zoom_start=13)
 # List to store the coordinates (latitude, longitude) for bounding box
 bounds = []
 
 # Loop through all the locations and add them to the map
-for location in locations:
+for location in home_locations:
     location = location.strip()  # Remove extra spaces
     h = geolocator.geocode(location)
     if h:
@@ -30,9 +45,26 @@ for location in locations:
         bounds.append(coords)
         
         # Add marker to the map
-        folium.Marker(coords, popup=location).add_to(map)
+        folium.Marker(coords, popup=location, icon=folium.Icon(icon='home')).add_to(map)
     else:
         print(f"Location '{location}' not found!")
+
+
+# Load companies from JSON files in the 'companies' directory
+companies_directory = 'companies'
+for filename in os.listdir(companies_directory):
+    if filename.endswith('.json'):
+        with open(os.path.join(companies_directory, filename)) as f:
+            company_data = json.load(f)
+            address = company_data.get('address')
+            if address:
+                h = geolocator.geocode(address)
+                if h:
+                    coords = [h.latitude, h.longitude]
+                    bounds.append(coords)
+                    
+                    # Add marker to the map for the company
+                    folium.Marker(coords, popup=f"{company_data['name']} - {company_data.get('website', 'No website')}").add_to(map)
 
 # Fit the map to the bounds of all locations
 if bounds:
